851bc9c66530c3e2232b7a6a4b7093734fee2f96
Enoxhh <enoshcherian4@gmail.com>
2026-04-20 11:36:00 +0530

    activity2

---
+++ activity2.py
@@ -0,0 +1,6 @@
+from datetime import datetime as DT
+Time = DT.now()
+print("Hour = ", Time.hour)
+print("minute = ", Time.minute)
+print("Second = ", Time.second)
+print("Microsecond = ", Time.microsecond)
